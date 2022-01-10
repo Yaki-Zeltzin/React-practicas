f21f49b649049eb04277b7276e6cc5bf27dd5103
zeltzin <zeltzin.rom96@gmail.com>
2022-01-10 12:31:03 -0600

    Componentes de clase y su ciclo de vida

--- Mi primera app/primera-app/src/Cupcake.js
+++ Mi primera app/primera-app/src/Cupcake.js
@@ -18,6 +18,22 @@ import React from "react";
 
 //**COMPONENTE DE CLASE**/
 class Cupcake extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            vendido: false
+        }//super
+
+        this.vender = this.vender.bind(this)
+    }//constructor
+
+    vender(){
+        this.setState({
+            vendido: true
+        })
+    }//vender
+
+
     render(){
         return(
             <div className="cupcake-item">
@@ -25,7 +41,10 @@ class Cupcake extends React.Component {
             <h2 className="cupcake-title">{this.props.nombre}</h2>
             <p className="cupcake-description">{`Sabor: ${this.props.sabor}`}</p>
             <p className="cupcake-price">{this.props.precio}</p>
-            <button className="cupcake-btn">Comprar</button>
+            <p className="cupcake-description"><b>Estado: </b>{this.state.vendido ? "Vendido" : "A la venta"}</p>
+            {   
+                !this.state.vendido && <button className="cupcake-btn" onClick={this.vender}>Comprar</button>
+            }
         </div>
         );
     }
